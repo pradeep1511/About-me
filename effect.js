e1d43a208cc19ccb269cd68c23d9d09fc30adaf6
Pradeep Chandra <pradeepchandra111@gmail.com>
2020-05-03 05:33:08 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -54,50 +54,50 @@ $('document').ready(function(){
 	function loopOne() {
 		var randleft = 100*Math.random();
 		var randtop = 50*Math.random();
-		$('#b1').animate({left:randleft,bottom:randtop},10000,function(){
+		$('#b1').animate({left:randleft,bottom:randtop},1000,function(){
 			loopOne();
 		});
 	}
 	function loopTwo() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b2').animate({left:randleft,bottom:randtop},10000,function(){
+		var randleft = 100*Math.random();
+		var randtop = 50*Math.random();
+		$('#b2').animate({left:randleft,bottom:randtop},1000,function(){
 			loopTwo();
 		});
 	}
 	function loopThree() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b3').animate({left:randleft,bottom:randtop},10000,function(){
+		var randleft = 100*Math.random();
+		var randtop = 50*Math.random();
+		$('#b3').animate({left:randleft,bottom:randtop},1000,function(){
 			loopThree();
 		});
 	}
 	function loopFour() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b4').animate({left:randleft,bottom:randtop},10000,function(){
+		var randleft = 100*Math.random();
+		var randtop = 50*Math.random();
+		$('#b4').animate({left:randleft,bottom:randtop},1000,function(){
 			loopFour();
 		});
 	}
 	function loopFive() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b5').animate({left:randleft,bottom:randtop},10000,function(){
+		var randleft = 100*Math.random();
+		var randtop = 50*Math.random();
+		$('#b5').animate({left:randleft,bottom:randtop},1000,function(){
 			loopFive();
 		});
 	}
 
 	function loopSix() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b6').animate({left:randleft,bottom:randtop},10000,function(){
+		var randleft = 100*Math.random();
+		var randtop = 50*Math.random();
+		$('#b6').animate({left:randleft,bottom:randtop},1000,function(){
 			loopSix();
 		});
 	}
 	function loopSeven() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b7').animate({left:randleft,bottom:randtop},10000,function(){
+		var randleft = 100*Math.random();
+		var randtop = 50*Math.random();
+		$('#b7').animate({left:randleft,bottom:randtop},1000,function(){
 			loopSeven();
 		});
 	}
